5972d83872a2e66c95b7805d2191caedf549b09b
nucuoidocduoc <35113054+nucuoidocduoc@users.noreply.github.com>
2023-04-19 00:38:55 +0700

    init

---
+++ OrderPoints/Command.cs
@@ -0,0 +1,49 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System.IO;
+using System.Linq;
+
+namespace OrderPoints
+{
+    public class Command
+    {
+        [CommandMethod("ExportPoints")]
+        public void ExportPoints()
+        {
+            var doc = Autodesk.AutoCAD.ApplicationServices.Core.Application.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var editor = doc.Editor;
+
+            using (Transaction acTrans = db.TransactionManager.StartTransaction())
+            {
+                // Request for objects to be selected in the drawing area
+                PromptSelectionResult acSSPrompt = editor.GetSelection();
+
+                // If the prompt status is OK, objects were selected
+                if (acSSPrompt.Status == PromptStatus.OK)
+                {
+                    var selectedObjects = acSSPrompt.Value.ToList<SelectedObject>();
+                    var entities = selectedObjects.Select(x => acTrans.GetObject(x.ObjectId, OpenMode.ForWrite));
+                    var firstSelectedObject = entities.FirstOrDefault(x => x is Polyline) as Polyline;
+                    var circles = entities.Where(x => x is Circle).Select(x => x as Circle).ToList();
+
+                    // Save the new object to the database
+
+                    var pointsOrdered = circles.Select(x => new { x.Center.X, x.Center.Y, Length = firstSelectedObject.GetDistAtPoint(x.Center) }).ToList().OrderBy(x => x.Length);
+
+                    using (StreamWriter writer = new StreamWriter(@"D:\abc.txt"))
+                    {
+                        foreach (var point in pointsOrdered)
+                        {
+                            writer.WriteLine($"{point.X}, {point.Y}, {point.Length}");
+                        }
+                    }
+                    acTrans.Commit();
+                }
+
+                // Dispose of the transaction
+            }
+        }
+    }
+}
---
+++ OrderPoints/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.AutoCAD.EditorInput;
+using System.Collections.Generic;
+
+namespace OrderPoints
+{
+    public static class Extensions
+    {
+        public static List<T> ToList<T>(this SelectionSet selectionSet)
+        {
+            var listElement = new List<T>();
+            foreach (T element in selectionSet)
+            {
+                listElement.Add(element);
+            }
+            return listElement;
+        }
+    }
+}
